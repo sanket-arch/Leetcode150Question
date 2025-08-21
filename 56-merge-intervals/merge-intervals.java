b078041134388745553a7d8fda0d3f958303746e
Sanket kumar <69155662+sanket-arch@users.noreply.github.com>
2025-08-21 22:40:54 +0530

    Time: 8 ms (85.54%) | Memory: 46.6 MB (73.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,31 +3,19 @@ class Solution {
         if(intervals.length == 1){
             return intervals;
         }
-        ArrayList<int[]> mergedInterval = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // it will take O(n)
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); // it will take O(nlogn)
+        int idx = 0;
         //O(n)
-        for(int i=1;i<n;i++){
-            int firstElement = intervals[i][0];
-            int lastElement = intervals[i][1];
-            if(start<=firstElement && firstElement<=end){
-                end = Math.max(lastElement, end);
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=intervals[idx][1]){
+                intervals[idx][1] = Math.max(intervals[idx][1], intervals[i][1]);
             } else{
-                mergedInterval.add(new int[]{start,end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+                idx++;
+                intervals[idx] = intervals[i];
             }
         }
 
-        if(start <= intervals[n-1][0] && intervals[n-1][0] <= end){
-            mergedInterval.add(new int[]{start, Math.max(end,intervals[n-1][1])});
-        } else{
-            mergedInterval.add(intervals[n-1]);
-        }
 
-        return mergedInterval.toArray(new int[mergedInterval.size()][]);
+        return Arrays.copyOf(intervals,idx+1);
     }
 }
