188f332e6c2d743324f28e0442a474101f2a21c5
Sanket kumar <69155662+sanket-arch@users.noreply.github.com>
2025-11-26 08:01:19 +0530

    Time: 4 ms (42.02%) | Memory: 51.7 MB (98.35%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,23 +3,20 @@ class Solution {
         int max_count = 0;
         int zero_count =0;
         int zero_idx = 0, start =0, end = 0;
-        while(end < nums.length){
+        while(start<= end && end < nums.length){
             if(nums[end] == 0) {
                 zero_count++;
             }
 
-            while(zero_count > k){
+            if(zero_count <= k ){
+                max_count = Math.max(end - start + 1, max_count);
+            } else if(zero_count > k){
                 if(nums[start]==0){
                     zero_count--;
                 }
                 start++;
             }
-
-
-            if(zero_count <= k ){
-                max_count = Math.max(end - start + 1, max_count);
-            } 
-            end++;  
+            end++;
         }
 
         return max_count;
